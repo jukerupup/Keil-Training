c831bafab3beb40c2150d908f3906feb22ff9d03
Yun <jukerupup@gmail.com>
2025-10-24 23:54:50 +0800

    Remove

--- Source/MyFUNC/Delay_F103.h
+++
@@ -1,6 +0,0 @@
-#include "stm32f10x.h"
-
-uint32_t GetHCLKFreq(void);
-void TIM2_Config (void);
-void Delay_us (uint16_t us);
-void Delay_ms (uint16_t ms);
--- Source/MyFUNC/UART.h
+++
@@ -1,6 +0,0 @@
-#include "stm32f10x.h"
-#define USART_BAUDRATE 115200
-uint16_t USART_CalcBRR(uint32_t PCLK, uint32_t BaudRate);
-void Uart1Config(void);
-void USART1_SendChar(char c);
-char USART1_ReadChar(void);
