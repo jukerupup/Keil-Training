6ffe6b85b6c95bb169a898e1ad3ae32ec3994516
Yun <jukerupup@gmail.com>
2025-10-25 14:21:50 +0800

    Add file Compiler:

---
+++ Source/Src/main.c
@@ -0,0 +1,48 @@
+#include "stm32f10x.h"
+#include "Delay_F103.h"
+#include "UART.h"
+
+void SysClockConfig(void)
+{
+	RCC->CR |= RCC_CR_HSERDY;
+	while(!(RCC->CR & RCC_CR_HSERDY));
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	FLASH->ACR |= FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_0;
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV1;
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+	RCC->CFGR |= RCC_CFGR_SW_HSI;
+}
+
+void GPIO_Config(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH &= ~(0xF << 20);
+	GPIOC->CRH |= 1<<20;
+}
+
+void delay(volatile uint32_t count)
+{
+    while (count--) {
+        __NOP();
+    }
+}
+
+int main(void)
+{
+	SysClockConfig();
+	//SystemInit();
+	GPIO_Config();
+	TIM2_Config();
+	Uart1Config();
+	while(1)
+	{
+		Delay_ms(1000);
+		//delay(1000000);
+		GPIOC->BSRR = (1 << (13 + 16));
+		Delay_ms(1000);
+		//delay(1000000);
+		GPIOC->BSRR = (1 << 13);
+		USART1_SendChar('C');
+	}
+}
