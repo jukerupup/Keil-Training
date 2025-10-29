b33a6370c0e8181187393a0efa4c12fb6564de3d
Yun <jukerupup@gmail.com>
2025-10-29 19:28:01 +0800

    Just modify code

--- Source/Src/syscalls.c
+++ Source/Src/syscalls.c
@@ -13,60 +13,62 @@
 
 void __io_putchar(char ch)
 {
-    USART1_SendChar(ch);
+	if ('\n' == ch)
+		USART1_SendChar('\r');
+	USART1_SendChar(ch);
 }
 
 int __io_getchar(void)
 {
-    return USART1_ReadChar();
+	return USART1_ReadChar();
 }
 
 int _write(int file, char *ptr, int len)
 {
-  for (int i = 0; i < len; i++)
-    __io_putchar(*ptr++);
-  return len;
+	for (int i = 0; i < len; i++)
+		__io_putchar(*ptr++);
+	return len;
 }
 
 int _read(int file, char *ptr, int len)
 {
-  for (int i = 0; i < len; i++)
-    *ptr++ = __io_getchar();
-  return len;
+	for (int i = 0; i < len; i++)
+		*ptr++ = __io_getchar();
+	return len;
 }
 
 int _close(int file)
 {
-  return -1;
+	return -1;
 }
 
 int _fstat(int file, struct stat *st)
 {
-  st->st_mode = S_IFCHR;
-  return 0;
+	st->st_mode = S_IFCHR;
+	return 0;
 }
 
 int _isatty(int file)
 {
-  return 1;
+	return 1;
 }
 
 int _lseek(int file, int ptr, int dir)
 {
-  return 0;
+	return 0;
 }
 
 void *_sbrk(ptrdiff_t incr)
 {
-  extern char _end; // Defined by the linker script
-  static char *heap_end;
-  char *prev_heap_end;
+	extern char _end; // Defined by the linker script
+	static char *heap_end;
+	char *prev_heap_end;
 
-  if (heap_end == 0)
-    heap_end = &_end;
+	if (heap_end == 0)
+		heap_end = &_end;
 
-  prev_heap_end = heap_end;
-  heap_end += incr;
+	prev_heap_end = heap_end;
+	heap_end += incr;
 
-  return (void *)prev_heap_end;
+	return (void *)prev_heap_end;
 }
