e6c8b53a33d4333bd37c5fabac4ad3c302eb29eb
Yun <jukerupup@gmail.com>
2025-11-01 21:13:05 +0800

    Just modif code

--- Source/Inc/debug_log.h
+++ Source/Inc/debug_log.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 
 typedef enum {
-    LOG_LEVEL_NONE=0,
-    LOG_LEVEL_ERROR,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_DEBUG,
+	LOG_LEVEL_NONE=0,
+	LOG_LEVEL_ERROR,
+	LOG_LEVEL_WARN,
+	LOG_LEVEL_INFO,
+	LOG_LEVEL_DEBUG,
 } LogLevel;
 
 /*
@@ -20,34 +20,34 @@ typedef enum {
 #define GLOBAL_LOG_LEVEL LOG_LEVEL_DEBUG
 #endif
 
-#define LOG_PRINT(level_str, module, fmt, ...)                          \
-    do{                                                                 \
-        printf("[%s][%s]" fmt "\n", level_str, module, ##__VA_ARGS__);  \
-    }while(0)
+#define LOG_PRINT(level_str, module, fmt, ...)							\
+	do{																	\
+		printf("[%s][%s]" fmt "\n", level_str, module, ##__VA_ARGS__);	\
+	}while(0)
 
-#define LOGE(fmt, ...)                                                      \
-    do {                                                                    \
-        if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_ERROR)                            \
-            LOG_PRINT("ERR", LOG_MODULE, fmt, ##__VA_ARGS__);               \
+#define LOGE(fmt, ...)													\
+	do {																\
+		if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_ERROR)						\
+			LOG_PRINT("ERR", LOG_MODULE, fmt, ##__VA_ARGS__);			\
     } while (0)
 
-#define LOGW(fmt, ...)                                                      \
-    do {                                                                    \
-        if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_WARN)                             \
-            LOG_PRINT("WRN", LOG_MODULE, fmt, ##__VA_ARGS__);               \
-    } while (0)
+#define LOGW(fmt, ...)													\
+	do {																\
+		if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_WARN)							\
+			LOG_PRINT("WRN", LOG_MODULE, fmt, ##__VA_ARGS__)			\
+	} while (0)
 
-#define LOGI(fmt, ...)                                                      \
-    do {                                                                    \
-        if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_INFO)                             \
-            LOG_PRINT("INF", LOG_MODULE, fmt, ##__VA_ARGS__);               \
-    } while (0)
+#define LOGI(fmt, ...)													\
+	do {																\
+		if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_INFO)							\
+			LOG_PRINT("INF", LOG_MODULE, fmt, ##__VA_ARGS__);			\
+	} while (0)
 
-#define LOGD(fmt, ...)                                                      \
-    do {                                                                    \
-        if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_DEBUG)                            \
-            LOG_PRINT("DBG", LOG_MODULE, fmt, ##__VA_ARGS__);               \
-    } while (0)
+#define LOGD(fmt, ...)													\
+	do {																\
+		if (GLOBAL_LOG_LEVEL >= LOG_LEVEL_DEBUG)						\
+			LOG_PRINT("DBG", LOG_MODULE, fmt, ##__VA_ARGS__);			\
+	} while (0)
 
 #endif
 
